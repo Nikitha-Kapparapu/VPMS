1013efd1058579627f0bbdb129bb6c0ffae23e65
VaishnaviReddy8864 <Vaishnavi.P@cognizant.com>
2025-07-02 18:03:06 +0530

    Reservation Completed

---
+++ reservation-service/src/main/java/com/parking/reservation_service/dto/ReservationRequestDTO.java
@@ -0,0 +1,16 @@
+package com.parking.reservation_service.dto;
+ 
+import lombok.*;
+ 
+import java.time.LocalDateTime;
+ 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationRequestDTO {
+    private Long userId;
+    private Long slotId;
+    private String vehicleNumber;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}
