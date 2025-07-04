93ef7ab1661cbf8aaa66dca8d11b8f599967b341
VaishnaviReddy8864 <Vaishnavi.P@cognizant.com>
2025-07-04 15:34:15 +0530

    Updated Services

--- billing-service/src/main/java/com/parking/billing_service/dto/CreateInvoiceRequest.java
+++ billing-service/src/main/java/com/parking/billing_service/dto/CreateInvoiceRequest.java
@@ -1,7 +1,10 @@
 package com.parking.billing_service.dto;
  
-import lombok.*;
 import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
  
 @Data
 @NoArgsConstructor
@@ -11,10 +14,10 @@ public class CreateInvoiceRequest {
     private Long reservationId;
     private Long logId; // Vehicle Log ID if applicable
     private String Type;    // e.g. "2W", "4W"
-    private long durationMinutes;  // duration from Vehicle Log or Reservation
+    // private long durationMinutes;  // duration from Vehicle Log or Reservation
     private LocalDateTime timestamp; // when billing is triggered
-    private Double amount;
+    // private Double amount;
     private String paymentMethod;  // UPI, CARD, CASH
-    private String status;         // PAID, UNPAID, CANCELLED
+    // private String status;         // PAID, UNPAID, CANCELLED
 }
  
