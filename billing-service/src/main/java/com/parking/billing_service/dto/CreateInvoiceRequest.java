cfc112f89c1d270f702d77474ada9c0710d02fd8
M <2407098@cognizant.com>
2025-07-04 15:27:10 +0530

    updated changes

--- billing-service/src/main/java/com/parking/billing_service/dto/CreateInvoiceRequest.java
+++ billing-service/src/main/java/com/parking/billing_service/dto/CreateInvoiceRequest.java
@@ -11,10 +11,10 @@ public class CreateInvoiceRequest {
     private Long reservationId;
     private Long logId; // Vehicle Log ID if applicable
     private String Type;    // e.g. "2W", "4W"
-    private long durationMinutes;  // duration from Vehicle Log or Reservation
+    // private long durationMinutes;  // duration from Vehicle Log or Reservation
     private LocalDateTime timestamp; // when billing is triggered
-    private Double amount;
+    // private Double amount;
     private String paymentMethod;  // UPI, CARD, CASH
-    private String status;         // PAID, UNPAID, CANCELLED
+    // private String status;         // PAID, UNPAID, CANCELLED
 }
  
